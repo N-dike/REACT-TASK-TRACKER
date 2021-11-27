89d9f71090b4e036ac8751ef02f7363987a0c1a2
N-dike <nnadike.dike@gmail.com>
2021-11-27 06:59:40 +0100

    master

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button';
+
+
+const Header = ({ title, onAdd, showAdd }) => {
+   
+    return ( <header className = 'header' >
+        <h1> { title } </h1> <
+        Button color={showAdd ? 'black' : 'blue'} 
+        text = {showAdd ? 'Close' : 
+        'Add'}
+     onClick={onAdd}/>
+
+        </header>  
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+//  CSS in react
+// const headingStyle = {
+//     color: 'grey', backgroundColor: 'lightblue',
+//     font: 'sans'
+// }
+export default Header
+
+// an if statement
+//{X ? 'YES' : 'No' }
+
+// YAHAWAH WA YAHAWASHI IBAD YA
